14cb21131ccaab811db9d88c4532bbb39c5229e4
byterose <>
2021-03-09 11:21:52 +0100

    Add condition

--- db/mongoose.js
+++ db/mongoose.js
@@ -214,7 +214,11 @@ const getIndex = async (req, res, next) => {
 const getLatestIndex = async (req, res, next) => {
   const index = await Index.find().select("timestamp price").exec();
 
-  res.json(index[index.length - 1]);
+  if (index.isEmpty()) {
+    res.json({});
+  } else {
+    res.json(index[index.length - 1]);
+  }
 };
 
 const getMedianRange = async (req, res, next) => {
